4275732a29cffa2881f54315893723b23bb83bad
Tejanshu <destejanshu@gmail.com>
2022-11-22 11:37:04 +0530

    Initial commit

---
+++ book-my-show/src/pages/Home.Page.js
@@ -0,0 +1,69 @@
+import React, {useState} from 'react'
+import DefaultLayoutHoc from "../layout/Default.layout";
+import EntertainmentCardSlider from"../components/Entertainment/EntertainmentCard.component"
+import HeroCarousel from"../components/HeroCarousel/HeroCarousel.component"
+import PosterSlider from"../components/PosterSlider/PosterSlider.component"
+import axios from 'axios';
+import { useEffect } from 'react';
+const HomePage = () => {
+  const [recommendedMovies, setRecommendedMovies] = useState([]);
+  const [premierMovies, setPremierMovies] = useState([]);
+  const [onlineStreamEvents, setOnlineStreamEvents] = useState([]);
+
+  useEffect(() => {
+
+    const requestPopularMovies = async() => {
+      const getPopularMovies = await axios.get('/movie/popular');
+      setRecommendedMovies(getPopularMovies.data.results);
+    };
+    requestPopularMovies();
+  }, []);
+
+  useEffect(() => {
+
+    const requestTopRatedMovies = async() => {
+      const getTopRatedMovies = await axios.get('/movie/top_rated');
+      setPremierMovies(getTopRatedMovies.data.results);
+    };
+    requestTopRatedMovies();
+  }, []);
+  
+  useEffect(() => {
+
+    const requestUpcomingMovies = async() => {
+      const getUpcomingMovies = await axios.get('/movie/upcoming');
+      setOnlineStreamEvents(getUpcomingMovies.data.results);
+    };
+    requestUpcomingMovies();
+  }, []);
+  
+  return (
+    <>
+      <HeroCarousel/>
+
+      <div className='container mx-auto px-4 md:px-12 my-8'>
+        <h1 className='text-2xl font-bold text-gray-800 sm:ml-3 ml-0 my-3'>The best of Entertainment</h1>
+        <EntertainmentCardSlider/>
+      </div>
+
+      <div className='container mx-auto px-4 md:px-12 my-8'>
+        <PosterSlider title="Recommended movies" subtitle="List of recommended movies" posters={recommendedMovies} isDark={false}/>
+      </div>
+
+      <div className="bg-premier-800 py-12">
+        <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+          <div className='hidden md:flex'>
+            <img src="https://assets-in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png" alt="Rupay" className="w-full h-full"/>
+          </div>
+        <PosterSlider title="Premiers" subtitle="Brand new releases every friday" posters={premierMovies} isDark={true}/>
+        </div>
+      </div>
+    
+      <div className='container mx-auto px-4 md:px-12 my-8'>
+        <PosterSlider title="Online Streaming Event" subtitle="" posters={onlineStreamEvents} isDark={false}/>
+      </div>
+    </>
+  )
+}
+
+export default DefaultLayoutHoc(HomePage);
